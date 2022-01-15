ac531f5d7366a2661bfa1fd7f6fa97bf8bad879d
mtgezgin <mehmetturgutgezgin@gmail.com>
2022-01-15 22:57:52 +0300

    Added updating cart functionality when meal is added.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
-const HeaderCartButton = () => {
+const HeaderCartButton = props => {
+
+    const cartContext = useContext(CartContext);
+
+    const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
+        return currentNumber + item.amount;
+    }, 0)
+
     return (
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>
-                3
+                {numberOfCartItems}
             </span>
         </button>
     )
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = props => {
     const price = `$${props.price.toFixed(2)}`;
+
+    const cartContext = useContext(CartContext)
+
+    const addToCartHandler = amount => {
+        cartContext.addItem({
+            id: props.id,
+            name: props.name,
+            amount,
+            price: props.price
+        });
+    }
     
     return (
         <li>
@@ -13,7 +25,7 @@ const MealItem = props => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm id={props.id} />
+                <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
             </div>
         </li>
     )           
